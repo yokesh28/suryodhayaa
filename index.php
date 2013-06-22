5c5a8da847cb238cb9957858edecfbafa1b8a274
screative2 <screative2@ubuntu>
2013-06-22 16:33:56 +0530

    Signed-off-by: screative2 <screative2@ubuntu>

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Suryodhayaa</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery-1.10.1.min.js"></script>
+
+</head>     
+<body>
+	<div id="panel1">
+		<img src="bg/bg.jpg" alt="Suryodhayaa">
+		<div class="wrapper">
+			<div class="log_wrp">
+				<a href="#" class="logo"><img src="img/logo-main.png"
+					alt="Suryodhayaa"> </a>
+				<ul class="social">
+					<li><a href="#" class="facebook"></a></li>
+					<li><a href="#" class="twitter"></a></li>
+					<li><a href="#" class="youtube"></a></li>
+				</ul>
+			</div> 
+			<ul class="menu_home">
+				<li><a href="#" class="home"><span>Home</span></a></li>
+				<li><a href="#" class="about"><span>About us</span></a></li>
+				<li><a href="#" class="process"><span>Process</span></a></li>
+				<li><a href="#" class="infras"><span>Infrastructure</span></a></li>
+				<li><a href="#" class="contact"><span>Contact Us</span></a></li>
+			</ul>
+		</div>
+
+	</div> 
+	<div id="panel2">
+		<div class="menu">
+			<a href="#"><img src="img/logo-main.png" width=300px> </a>
+			<ul class="menu_float">
+				<li><a href="#" class="home">Home</a></li>
+				<li><a href="#" class="about">About us</a></li>
+				<li><a href="#" class="process">Process</a></li>
+				<li><a href="#" class="infras">Infrastructure</a></li>
+				<li><a href="#" class="contact">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+
+
+</body>
+</html>
